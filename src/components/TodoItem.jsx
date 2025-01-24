f9bafbe74bfbd4d3fc8f58f3728cbf3db0f9e460
espadv69 <aitorjob69@gmail.com>
2025-01-24 12:41:01 +0100

    Add delete functionality to TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,12 @@
-const TodoItem = ({ task }) => {
+const TodoItem = ({ task, onDeleteTask }) => {
   const priorityColors = {
     low: '#edf7ed',
     medium: '#fffbe6',
     high: '#ffe6e6',
+  }
+
+  const handleDeleteTask = () => {
+    onDeleteTask(task)
   }
 
   return (
@@ -12,6 +16,9 @@ const TodoItem = ({ task }) => {
     >
       <span>{task.task}</span>
       <span>{task.priority.toUpperCase()}</span>
+      <button type="button" onClick={handleDeleteTask}>
+        Delete
+      </button>
     </li>
   )
 }
