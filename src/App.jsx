1feee04a60d20f3118d2eda89041e7f109254094
espadv69 <aitorjob69@gmail.com>
2025-01-24 12:21:49 +0100

    Add App component to render TodoForm in main application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import TodoForm from './components/TodoForm'
+
+const App = () => {
+  return (
+    <main>
+      <TodoForm />
+    </main>
+  )
+}
+
+export default App
