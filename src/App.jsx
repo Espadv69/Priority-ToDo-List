35aa2a830f153cb49070da372ff5ad76fde0867f
espadv69 <aitorjob69@gmail.com>
2025-01-24 12:24:37 +0100

    Add task management functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 import TodoForm from './components/TodoForm'
 
 const App = () => {
+  const [tasks, setTasks] = useState([])
+
+  const handleAddTask = (newTask) => {
+    setTasks((prevTask) => [...prevTask, newTask])
+  }
   return (
     <main>
       <h1>Todo List with Priority</h1>
-      <TodoForm />
+      <TodoForm onAddTask={handleAddTask} />
       {/* Todo: Show tasksList */}
     </main>
   )
