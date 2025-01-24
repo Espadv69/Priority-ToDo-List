e434193090aad410f40d49af5eb1ab1de6644081
espadv69 <aitorjob69@gmail.com>
2025-01-24 12:30:47 +0100

    Fix TodoItem component to render task with priority color coding

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,7 +5,15 @@ const TodoItem = ({ task }) => {
     high: '#ffe6e6',
   }
 
-  return
+  return (
+    <li
+      className="li-todoItem"
+      style={{ backgroundColor: priorityColors[task.priority] }}
+    >
+      <span>{task.task}</span>
+      <span>{task.priority.toUpperCase()}</span>
+    </li>
+  )
 }
 
 export default TodoItem
