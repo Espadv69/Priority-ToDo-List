be58c8b637d78324ba4967b830f4a3d25b1b448e
espadv69 <aitorjob69@gmail.com>
2025-01-24 12:10:11 +0100

    Add task and priority input fields to TodoForm component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react'
 
 const TodoForm = ({ onAddTask }) => {
-  
+  const [task, setTask] = useState('')
+  const [priority, setPriority] = useState('low')
+
+  return (
+    <form>
+      <label>
+        Task:
+        <input type="text" />
+      </label>
+
+      <label>
+        Priority:
+        <input type="text" />
+      </label>
+
+      <button>Add Task</button>
+    </form>
+  )
 }
