eae6c840afa53f5667839f814ee7f2914fcb5dce
espadv69 <aitorjob69@gmail.com>
2025-01-24 12:45:28 +0100

    Pass onDeleteTask prop to TodoItem component for task deletion

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,22 @@
 import TodoItem from './TodoItem'
 
-const TodoList = ({ tasks }) => {
+const TodoList = ({ tasks, onDeleteTask }) => {
   if (tasks.length === 0) {
     return <p className="p-todoList">No tasks added yet!</p>
+  }
+
+  const deleteTask = (index) => {
+    const filteredTasks = tasks.filter((task) => task.index !== index)
   }
 
   return (
     <ul className="ul-todoList">
       {tasks.map((task, index) => (
-        <TodoItem key={index} task={task} />
+        <TodoItem
+          key={index}
+          task={task}
+          onDeleteTask={() => onDeleteTask(index)}
+        />
       ))}
     </ul>
   )
